d71f8830bec96cd254dae0fbf81e06c54da78696
Jon Friedman <jfriedman@motherjones.com>
2014-03-03 12:56:54 -0800

    WEB-321 added tests for chooseTemplate; very sparse template mapper

--- dev/test/application_tests.js
+++ dev/test/application_tests.js
@@ -10,5 +10,62 @@ module.exports = (function() {
       ok(unit_tests.HiveMind, "it's here");
     }]);
 
+  unit_tests.tests.push(
+    ["test template chooser with a good component",
+     function() {
+       expect(1);
+       var article_component = {
+	 schema_name: 'article',
+	 content_type: 'text/x-markdown'
+       };
+
+       strictEqual(unit_tests.HiveMind.chooseTemplate(article_component),
+	 	   'article',
+		   'template chooser returns correct template name for a'
+		   + ' correct schema and content type pair');
+     }
+    ]
+  );
+
+  unit_tests.tests.push(
+    ["test template chooser with a bad schema name",
+     function() {
+       expect(1);
+       var article_component = {
+	 schema_name: 'srticle',
+	 content_type: 'text/x-markdown'
+       };
+
+       strictEqual(unit_tests.HiveMind.chooseTemplate(article_component),
+		   null,
+		   'template chooser returns null for an unknown schema/content'
+		   + ' type pair');
+     }
+    ]
+  );
+
+  unit_tests.tests.push(
+    ["test template chooser with a bad content type",
+     function() {
+       expect(1);
+       var article_component = {
+	 schema_name: 'article',
+	 content_type: 'image/jpeg'
+       };
+
+       strictEqual(unit_tests.HiveMind.chooseTemplate(article_component),
+	   	   null,
+		   'template chooser returns null for an unknown schema/content'
+		   + ' type pair');
+     }
+    ]
+  );
+
+  unit_tests.tests.push(["test dust html filter", function() {
+    expect(1);
+    ok(false, "test not implemented yet");
+  }]);
+
+
     return unit_tests;
 })();
