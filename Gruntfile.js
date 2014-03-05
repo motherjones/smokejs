970cc2e78b30c15a240dc48a9b6a96d75e38c525
Mikela Clemmons <glassresistor@gmail.com>
2014-03-05 14:14:44 -0800

    fixed 4 spaces issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,142 +1,113 @@
 'use strict';
-
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
-    // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     concat: {
       options: {
         banner: '<%= banner %>',
         stripBanners: true
-      }    },
+      }
+    },
     browserify: {
-        dist: {
-            files: {
-                'build/js/smoke.js': ['dev/js/main.js']
-            }
-        },
-        test: {
-            files: {
-                'build/test/smoke_test.js': ['dev/test/all.js']
-            }
-        },
+      dist: { files: { 'build/js/smoke.js': ['dev/js/main.js'] } },
+      test: { files: { 'build/test/smoke_test.js': ['dev/test/all.js'] } }
     },
     less: {
       development: {
         files: {
-          "build/css/<%= pkg.name %>.css": ["dev/css/**/*.less", "dev/css/**/*.css"]
+          'build/css/<%= pkg.name %>.css': [
+            'dev/css/**/*.less',
+            'dev/css/**/*.css'
+          ]
         }
       },
       production: {
-        options: {
-          yuicompress: true
-        },
+        options: { yuicompress: true },
         files: {
-          "build/css/<%= pkg.name %>.min.css": ["dev/css/*.less", "dev/css/*.css"]
+          'build/css/<%= pkg.name %>.min.css': [
+            'dev/css/*.less',
+            'dev/css/*.css'
+          ]
         }
       }
     },
     uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
+      options: { banner: '<%= banner %>' },
       dist: {
         src: 'build/js/smoke.js',
         dest: 'build/js/smoke.min.js'
-      },
+      }
     },
-    qunit: {
-        all: {
-          options: {
-            urls: [
-              'http://localhost:9001/smoke_test.html'
-            ]
-          }
-        }
-    },
+    qunit: { all: { options: { urls: ['http://localhost:9001/smoke_test.html'] } } },
     jshint: {
       gruntfile: {
-        options: {
-          jshintrc: '.jshintrc'
-        },
+        options: { jshintrc: '.jshintrc' },
         src: 'Gruntfile.js'
       },
       src: {
-        options: {
-          jshintrc: 'dev/js/.jshintrc'
-        },
-        src: ['dev/js/**/*.js', '!dev/js/libs/**/*.js']
+        options: { jshintrc: 'dev/js/.jshintrc' },
+        src: [
+          'dev/js/**/*.js',
+          '!dev/js/libs/**/*.js'
+        ]
       },
       test: {
-        options: {
-          jshintrc: 'dev/test/.jshintrc'
-        },
+        options: { jshintrc: 'dev/test/.jshintrc' },
         src: ['dev/test/*.js']
-      },
+      }
     },
     cssmin: {
       minify: {
         expand: true,
         cwd: 'build/css/',
-        src: ['/*.css',],
+        src: ['/*.css'],
         dest: 'build/css',
         ext: '.css'
-      },
+      }
     },
     connect: {
-        server: {
-            options: {
-                port: 9001,
-                base: 'build',
-                middleware: function(connect, options) {
-                    var less = require('less-middleware');
-                    return [
-                        less({ src: options.base }),
-                        connect.static(options.base),
-                        connect.directory(options.base)
-                    ];
-                }
-            }
+      server: {
+        options: {
+          port: 9001,
+          base: 'build',
+          middleware: function (connect, options) {
+            var less = require('less-middleware');
+            return [
+              less({ src: options.base }),
+              connect.static(options.base),
+              connect.directory(options.base)
+            ];
+          }
         }
+      }
     },
     htmlmin: {
-        build: {
-            options: {                                 // Target options
-                removeComments: true,
-                collapseWhitespace: true
-            },
-            files: {
-                'build/index.html': 'dev/index.html',     // 'destination': 'source'
-                'build/smoke_test.html': 'dev/test/smoke_test.html',     // 'destination': 'source'
-            }
+      build: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'build/index.html': 'dev/index.html',
+          'build/smoke_test.html': 'dev/test/smoke_test.html'
         }
+      }
     },
     dust: {
-        defaults: {
-            files: {
-                'build/js/dust_templates.js': ["dev/templates/**/*.dust"]
-            },
-            options: {
-                runtime: false,
-                basePath: 'dev/templates',
-                wrapper: 'commonjs',
-                  wrapperOptions: {
-                    returning: "dust",
-                    deps: {
-                      dust: "dustjs-linkedin"
-                    }
-                  }
-
-            }
+      defaults: {
+        files: { 'build/js/dust_templates.js': ['dev/templates/**/*.dust'] },
+        options: {
+          runtime: false,
+          basePath: 'dev/templates',
+          wrapper: 'commonjs',
+          wrapperOptions: {
+            returning: 'dust',
+            deps: { dust: 'dustjs-linkedin' }
+          }
         }
+      }
     },
     watch: {
       gruntfile: {
@@ -145,11 +116,19 @@ module.exports = function(grunt) {
       },
       src: {
         files: '<%= jshint.src.src %>',
-        tasks: ['jshint:src', 'browserify', 'qunit']
+        tasks: [
+          'jshint:src',
+          'browserify',
+          'qunit'
+        ]
       },
       templates: {
         files: 'dev/templates/**/*.dust',
-        tasks: ['dust', 'browserify', 'qunit']
+        tasks: [
+          'dust',
+          'browserify',
+          'qunit'
+        ]
       },
       css: {
         files: 'dev/css/**/*.less',
@@ -161,11 +140,14 @@ module.exports = function(grunt) {
       },
       test: {
         files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'browserify:test', 'qunit']
-      },
-    },
+        tasks: [
+          'jshint:test',
+          'browserify:test',
+          'qunit'
+        ]
+      }
+    }
   });
-
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
@@ -178,14 +160,52 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-dust');
   grunt.loadNpmTasks('grunt-browserify');
-
   // Default task.
-  grunt.registerTask('default', ['dust', 'jshint', 'browserify', 'concat', 'uglify', 'less', 'htmlmin', 'connect', 'qunit']);
-  grunt.registerTask('min', ['dust', 'browserify', 'concat', 'uglify', 'cssmin', 'htmlmin']);
-  grunt.registerTask('test', ['browserify:test', 'connect', 'qunit', 'htmlmin']);
+  grunt.registerTask('default', [
+    'dust',
+    'jshint',
+    'browserify',
+    'concat',
+    'uglify',
+    'less',
+    'htmlmin',
+    'connect',
+    'qunit'
+  ]);
+  grunt.registerTask('min', [
+    'dust',
+    'browserify',
+    'concat',
+    'uglify',
+    'cssmin',
+    'htmlmin'
+  ]);
+  grunt.registerTask('test', [
+    'browserify:test',
+    'connect',
+    'qunit',
+    'htmlmin'
+  ]);
   grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('css', ['less', 'cssmin']);
-  grunt.registerTask('fab', ['dust', 'browserify', 'concat', 'uglify', 'less', 'cssmin', 'htmlmin' ]);
-  grunt.registerTask('serve', ['dust', 'browserify', 'less:development', 'htmlmin', 'connect', 'watch']);
-
+  grunt.registerTask('css', [
+    'less',
+    'cssmin'
+  ]);
+  grunt.registerTask('fab', [
+    'dust',
+    'browserify',
+    'concat',
+    'uglify',
+    'less',
+    'cssmin',
+    'htmlmin'
+  ]);
+  grunt.registerTask('serve', [
+    'dust',
+    'browserify',
+    'less:development',
+    'htmlmin',
+    'connect',
+    'watch'
+  ]);
 };
