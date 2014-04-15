d47d85a3c95c5e2f69fb7e18000ff1c44488e4be
Ben Breedlove <benbreedlove@gmail.com>
2014-04-15 11:45:06 -0700

    move router differences for edit into main-edit file

--- dev/js/main_edit.js
+++ dev/js/main_edit.js
@@ -10,12 +10,42 @@
 
 (function() {
   var Backbone = require('backbone');
-  var Router = require('./router_edit');
+  var Router = require('./router');
+  var Article = require('./article_edit');
   var $ = require('jquery');
 
 
-  new Router({
+  var router = new Router({
   });
+
+  router.routes["edit/article/:slug"] = "editArticle";
+
+  router.editArticle = function(slug) {
+    var articleModel = new Article.Model({ id: slug });
+    var articleView = new Article.View({ model: articleModel });
+    console.log(articleView);
+    // write what to do when editing an article here
+  };
+
+  router.routes["edit/topic/:slug"] = "curateTopic";
+  router.routes["edit/section/:slug"] = "curateTopic";
+  router.routes["edit/list/:slug"] = "curateTopic";
+
+  router.curateTopic = function(slug) {
+    var articleCollection = new Article.Collection({ id: slug });
+    var articleCollectionView 
+      = new Article.CollectionView({ collection: articleCollection });
+    // write what to do when curating a single list here
+    console.log(articleCollectionView);
+  };
+
+  router.routes["edit/splashpage/:slug"] = "curateSplashpage";
+
+  router.curateSplashPage = function(slug) {
+    console.log(slug);
+    // write what to do when curating a list of lists here
+  };
+
 
   $(document).ready(function() {
     Backbone.history.start();
