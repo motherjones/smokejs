0d6154da7bdf7b2289e654c845340d0bfe49f1c8
Mikela Clemmons <glassresistor@gmail.com>
2014-05-06 15:25:02 -0700

    updated tests so all fail

--- dev/test/all.js
+++ dev/test/all.js
@@ -1,7 +1,5 @@
 /*global require */
 
-//require('./basic_tests');
-//require('./hivemind_tests');
-//require('./story_tests');
-//require('./ad_tests');
+require('./story_tests');
+require('./ad_tests');
 require('./markdown_tests');
