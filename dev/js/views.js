d9ee5762d51addf803ca6bef7fb8789ad7d3f044
Ben Breedlove <benbreedlove@gmail.com>
2014-05-07 12:53:25 -0700

    pass linting

--- dev/js/views.js
+++ dev/js/views.js
@@ -1,25 +1,38 @@
-export.display_main_content = function(match, callback) {
-  var promise = new $.Deferred();
-  API.load('/mirrors/component/' + match.params.slug, function(data) {
-    render(match.params.schema, data, function(html) {
-      $('body').html(html);
-      Ad.reload(data.keywords);
-      callback();
-      promise.resove();
-    });
-  });
-  return promise;
-};
+'use strict';
 
-export.display_homepage = function(match, callback) {
-  var promise = new $.Deferred();
-  API.load('/homepage', function(data) {
-    render('homepage', data, function(html) {
-      $('body').html(html);
-      Ad.reload(data.keywords);
-      callback();
-      promise.resove();
+module.exports = (function() {
+  var $ = require('jquery');
+  var Ad = require('./ad');
+  var API = require('./api');
+  var render = require('./render');
+
+  var views = {};
+
+  views.display_main_content = function(match, callback) {
+    var promise = new $.Deferred();
+    API.load('/mirrors/component/' + match.params.slug, function(data) {
+      render(match.params.schema, data, function(html) {
+        $('body').html(html);
+        Ad.reload(data.keywords);
+        callback();
+        promise.resove();
+      });
     });
-  });
-  return promise;
-};
+    return promise;
+  };
+
+  views.display_homepage = function(match, callback) {
+    var promise = new $.Deferred();
+    API.load('/homepage', function(data) {
+      render('homepage', data, function(html) {
+        $('body').html(html);
+        Ad.reload(data.keywords);
+        callback();
+        promise.resove();
+      });
+    });
+    return promise;
+  };
+
+  return views;
+})();
