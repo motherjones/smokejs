550c2b7b10f7cbd6d52709bb0d28e3af52658e99
Mikela Clemmons <glassresistor@gmail.com>
2014-05-07 12:44:52 -0700

    fixed typo

--- dev/js/router.js
+++ dev/js/router.js
@@ -20,7 +20,7 @@ module.exports = (function() {
     });
   };
 
-  router.addRoute('^[^/]+*/[^/]+$', views.display_homepage
+  router.addRoute('^[^/]+*/[^/]+$', views.display_homepage);
   router.addRoute('^$', views.display_homepage);
 
   return router;
