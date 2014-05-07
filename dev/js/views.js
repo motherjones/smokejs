0a0a44e2cb926442e91e72e428d10590f9e6dcb3
Ben Breedlove <benbreedlove@gmail.com>
2014-05-07 12:20:34 -0700

    add promises to views

--- dev/js/views.js
+++ dev/js/views.js
@@ -1,19 +1,25 @@
 export.display_main_content = function(schema, slug, callback) {
+  var promise = new $.Deferred();
   API.load('/mirrors/component/' + slug, function(data) {
     render(schema, data, function(html) {
       $('body').html(html);
       Ad.reload(data.keywords);
       callback();
+      promise.resove();
     });
   });
+  return promise;
 };
 
 export.display_homepage = function(callback) {
+  var promise = new $.Deferred();
   API.load('/homepage', function(data) {
     render('homepage', data, function(html) {
       $('body').html(html);
       Ad.reload(data.keywords);
       callback();
+      promise.resove();
     });
   });
+  return promise;
 };
