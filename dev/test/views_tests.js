11f4de7e8efc649e214ab8b77529a1a90f37ff20
Mikela Clemmons <glassresistor@gmail.com>
2014-05-13 15:26:13 -0700

    only two tests fail now and ones expected

--- dev/test/views_tests.js
+++ dev/test/views_tests.js
@@ -5,27 +5,22 @@ var views = require('../js/views');
 var EnvConfig = require('../js/config');
 var utils = require('./utils');
 var response_peter = require('./fixtures/author/peter.json');
-var response_peter = require('./fixtures/homepage.json');
+var response_homepage = require('./fixtures/homepage.json');
 
 var match_mock = { params: {} };
 test( "test display_main_content", function(t) {
-  t.plan(3);
+  t.plan(2);
   var match = match_mock;
   match.params.slug = 'peter';
   match.params.schema = 'author';
 
-  var server = utils.mock_component(match.params.slug, response);
-  var callback = function(data) {
-    t.ok(data, 'data is returned');
-    t.equal(data['slug'], slug, 'slug is returned');
-    server.restore();
-    t.end();
-  };
-
+  var server = utils.mock_component(match.params.slug, response_peter);
   var promise = views.display_main_content(match, function(data, html) {
     t.equal( data.content_type, "text/x-markdown",
       'display main content has a callback which provides the data of the object loaded'
     );
+    server.restore();
+    t.end();
   });
 
   t.equal(
@@ -43,6 +38,7 @@ test( "test display_homepage", function(t) {
     t.equal( data.metadata.title, "Mother Jones Home Page",
       'display homepage has a callback which provides the data of the object loaded'
     );
+    t.end();
   });
 
   t.equal(
