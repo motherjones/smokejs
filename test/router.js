ac85a4af36fd29a4d69dca67ea74cda824324787
Mikela Clemmons <glassresistor@gmail.com>
2014-05-28 13:10:41 -0700

    added hack to force history tests to work, we should at somepoint look into that

--- test/router.js
+++ test/router.js
@@ -3,18 +3,21 @@ var test = require('tape');
 var router = require('../js/router');
 var $ = require('jquery');
 var Ad = require('../js/ad');
+
 test('ad loaded', function(t) {
   t.plan(1);
   t.ok(Ad, 'ad lib loaded');
 });
+
 test('router loaded', function(t) {
   t.plan(2);
   t.ok(router, 'router lib loaded');
   t.ok(router.match, 'router match function loaded');
 });
+
 test( "test router pop", function(t) {
   t.plan(2);
-  router.pop('/article/obama-drone-pakistan-us-citizen-leak-killing');
+  router.pop('/article/test');
   var test_callback = function() {
     t.ok(true, 'pop called the callback function');
   };
@@ -22,6 +25,7 @@ test( "test router pop", function(t) {
   t.ok(true, 'route added successfully');
   router.pop('test');
 });
+
 test( "test router callbacks", function(t) {
   t.plan(6);
   t.equal(null, router.callback(),
@@ -34,19 +38,23 @@ test( "test router callbacks", function(t) {
   t.ok(Ad, 'ad lib loaded');
   t.equal(Ad.key, 'keyword teststring', 'browser callback sets ad keywords');
   Ad.key = '';
+  window.history.pushState(0, 0, location.pathname); //TODO: Figure out why this is required.
   var current_history_length = history.length;
   router.addRoute("/loltest", function() {});
   var fakelink = $('<a href="#/loltest"></a>').click(router.browserClick);
   var promise = fakelink.click();
+  var oldLocation = location.pathname;
   $.when(promise).done(function() {
-    t.equal(current_history_length + 1, history.length,
+    t.equal(history.length, current_history_length + 1,
       'click event ads to history'
     );
     t.equal('/loltest', location.pathname,
       'click event takes browser url to link'
     );
+    window.history.back();
   });
 });
+
 test( "test switching to browser mode", function(t) {
   t.plan(4);
   router.browserClick = function() {
