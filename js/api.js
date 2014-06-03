89e5672c5c0bd28174ba6c3c1d7f7acd1b748316
Ben Breedlove <benbreedlove@gmail.com>
2014-06-03 16:45:57 -0700

    fix lingering uses of when and state for jquery promises, expose mock server issue

--- js/api.js
+++ js/api.js
@@ -5,8 +5,10 @@ var Promise = require('promise-polyfill');
 
 exports.component = function(slug, callback) {
   var promise = new Promise(function(resolve, reject) {
-    if ( typeof(Storage)!=="undefined" && localStorage.getItem(slug) ) {
-      callback(localStorage.getItem(slug));
+    if ( typeof(Storage)!=="undefined" && localStorage.getItem(slug) &&
+        localStorage.getItem(slug) !== '[object Object]'
+      ) {
+      callback(JSON.parse(localStorage.getItem(slug)));
       resolve();
     } else {
       $.getJSON(EnvConfig.MIRRORS_URL + 'component/' + slug,
@@ -14,7 +16,7 @@ exports.component = function(slug, callback) {
           if ( typeof(Storage)!=="undefined" ) {
             localStorage.setItem(slug, data);
           }
-          callback(data);
+          callback(JSON.parse(data));
           resolve();
         }
       );
