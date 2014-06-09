be81577e5b1eecb088b98bdf5f1f8335c4dd8fe1
Mikela Clemmons <glassresistor@gmail.com>
2014-06-09 10:42:28 -0700

    stash changes breaking

--- test/api.js
+++ test/api.js
@@ -5,28 +5,34 @@ var test = require('tape');
 var response = require('./fixtures/article/1.json');
 var utils = require('./utils');
 
-test("test component api", function(t) {
-  t.plan(2);
-  var slug = 'test';
-  var server = utils.mock_component(slug, response);
-  var callback = function(data) {
-    t.ok(data, 'data is returned');
-    t.equal(data['slug'], slug, 'slug is returned');
-    server.restore();
-    t.end();
-  };
-  var promise = api.component(slug, callback);
-  $.when(promise).done(function() {
-    //maybe a test here?
-  });
-});
+module.exports = {
+  "beforeEach": function() {
+
+  },
+  "component api": {
+    "get" : function() {
+      var slug = 'test';
+      var server = utils.mock_component(slug, response);
+      var callback = function(data) {
+        should.exist.ok(data, 'data is returned');
+        t.equal(data['slug'], slug, 'slug is returned');
+        server.restore();
+        t.end();
+      };
+      var promise = api.component(slug, callback);
+      $.when(promise).done(function() {
+        //maybe a test here?
+      });
+    }
+  }
+}
 
 test("test component api's use of localstorage", function(t) {
   t.plan(2);
   var localSlug = 'localtest';
   var localResponse = 'test local data';
   localStorage.setItem(localSlug, localResponse);
-  var promise = api.component(localSlug, function(response) { 
+  var promise = api.component(localSlug, function(response) {
     t.equal(response, localResponse, 'component pulls from localstorage');
     t.end();
   });//pulls from local are sync, thank goodness
