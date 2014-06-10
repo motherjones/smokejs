cd195b2324e672609eaba499258288da4b5e3ec4
Mikela Clemmons <glassresistor@gmail.com>
2014-06-10 16:18:42 -0700

    got router tests working

--- test/router.js
+++ test/router.js
@@ -3,18 +3,20 @@ var test = require('tape');
 var router = require('../js/router');
 var $ = require('jquery');
 var Ad = require('../js/ad');
+var should = require('should');
 
-describe("Ads", function() {
+
+describe("router properties", function() {
   it('ad loaded', function(done) {
     should.exist(Ad);
     done();
   });
-  it('router loaded', function(d) {
+  it('router loaded', function(done) {
     should.exist(router);
     router.should.have.property('match');
     done();
   });
-  it("router pop calls callback", function() {
+  it("router pop calls callback", function(done) {
     var test_callback = function() {
       done();
     };
@@ -24,55 +26,68 @@ describe("Ads", function() {
 });
 
 
+describe("router callbacks", function() {
+  it('router starts w/ callback doing nothing', function(done) {
+    should(router.callback()).eql(null, 'callback is not set');
+    done();
+  });
 
-test( "test router callbacks", function(t) {
-  t.equal(null, router.callback(),
-    'router starts w/ callback doing nothing'
-  );
-  router.browserCallback({ keywords: 'keyword teststring' }, 'lolno');
-  t.equal( $('body').html(), 'lolno',
-    'Browser callback sets the body html'
-  );
-  t.ok(Ad, 'ad lib loaded');
-  t.equal(Ad.key, 'keyword teststring', 'browser callback sets ad keywords');
-  Ad.key = '';
-  window.history.pushState(0, 0, location.pathname); //TODO: Figure out why this is required.
-  var current_history_length = history.length;
-  router.addRoute("/loltest", function() {});
-  var fakelink = $('<a href="#/loltest"></a>').click(router.browserClick);
-  var promise = fakelink.click();
-  var oldLocation = location.pathname;
-  $.when(promise).done(function() {
-    t.equal(history.length, current_history_length + 1,
-      'click event ads to history'
-    );
-    t.equal('/loltest', location.pathname,
-      'click event takes browser url to link'
-    );
+  beforeEach(function(done) {
+    router.browserCallback({ keywords: 'keyword teststring' }, 'lolno');
+    done();
+  });
+  it('callback sets the body html', function(done) {
+    should($('body').html()).eql('lolno');
+    done();
+  });
+  after(function(done) {
+    Ad.key = '';
+    done();
+  });
+  it('sets browser Ad callback and keys', function(done) {
+    should.exist(Ad);
+    Ad.should.have.property('key', 'keyword teststring');
+    done();
+  });
+  before(function() {
+    window.history.pushState(0, 0, location.pathname); //TODO: Figure out why this is required.
+  });
+  after(function() {
     window.history.back();
+  });
+  it("click event adds to history", function(done) {
+    var current_history_length = history.length;
+    var uri = "/loltest";
+    router.addRoute(uri, function() {});
+    var fakelink = $('<a href="#/loltest"></a>').click(router.browserClick);
+    var promise = fakelink.click();
+    var oldLocation = location.pathname;
+    $.when(promise).done(function() {
+      should(current_history_length + 1).eql(history.length);
+      uri.should.eql(location.pathname);
+      done();
+    });
   });
 });
 
-test( "test switching to browser mode", function(t) {
-  t.plan(4);
-  router.browserClick = function() {
-    t.ok(true, 'after runing browserStart, clicks on links in body run browserClick');
-  };
-  router.pop = function() {
-    var promise = new $.Deferred();
-    //FIXME running this pop should happen where we decide we're in a browser
-    t.ok(true, 'starting browser mode runs pop');
-    promise.resolve();
-    return promise;
-  };
-  router.browserStart();
-  t.deepEqual(router.browserCallback, router.callback,
-    'browser start sets callback to browser specific callback'
-  );
-  var fakelink = $('<a href="#/loltest" id="fakelink"></a>');
-  var body = $('body');
-  t.ok(body, 'body found w/ jquery');
-  body.append(fakelink);
-  $('#fakelink')
-    .click();
+
+describe("browserStart", function() {
+  it("sets broserClick", function(done) {
+    router.browserClick = function() {
+      done();
+    };
+    router.pop = function() {
+      var promise = new $.Deferred();
+      //FIXME running this pop should happen where we decide we're in a browser
+      promise.resolve();
+      return promise;
+    };
+    router.browserStart();
+    should(router.browserCallback).eql(
+      router.callback, 'sets router.callback to browserCallback');
+    var fakelink = $('<a href="#/loltest" id="fakelink"></a>');
+    var body = $('body');
+    body.append(fakelink);
+    $('#fakelink').click();
+  });
 });
