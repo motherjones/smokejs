d3e8e6617142bf06efe66fd370c9b6143ce72ef0
Mikela Clemmons <glassresistor@gmail.com>
2014-07-03 16:25:24 -0700

    added tests for Data get

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-exports.MIRRORS_DOMAIN = 'http://localhost:9001/';
-exports.MIRRORS_URI = 'mirrors/';
+exports.MIRRORS_DOMAIN = 'http://localhost:9001';
+exports.MIRRORS_URI = '/mirrors/';
 exports.MIRRORS_URL = exports.MIRRORS_DOMAIN + exports.MIRRORS_URI;
 
 exports.AD_LOCATION = 'http://mj-tech.s3.amazonaws.com/ad_w_intersitial.html';
--- test/api.js
+++ test/api.js
@@ -25,24 +25,24 @@ describe("component api", function() {
 });
 
 
-/*
 describe("data api", function() {
   describe("get", function() {
     it("returns data from mirrors", function(done) {
       var slug = 'test';
-      var server = utils.mock_component_data(slug, response);
+      var server = utils.mock_component(slug, {}, {
+        'content-type': 'text/x-markdown',
+        'response': 'DATA'
+      });
       var callback = function(data) {
         server.restore();
         should.exist(data);
-        data.should.have.property('slug', slug);
+        data.should.be.exactly('DATA');
       };
-      var component = new api.Component(slug);
-      component.get(callback).then(function() {
-        should(component).have.property('metadata');
-        should(component).have.property('attributes');
+      var data_uri = '/mirrors/component/' + slug +'/data';
+      var data = new api.Data(data_uri);
+      data.get(callback).then(function() {
         done();
       });
     });
   });
 });
-*/
