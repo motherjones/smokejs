ebac882364567c6799f3515f0bc9c164eb457f3b
Ben Breedlove <benbreedlove@gmail.com>
2014-07-09 13:49:29 -0700

    add a bunch of documentation

--- js/edit_views.js
+++ js/edit_views.js
@@ -1,7 +1,18 @@
 var views = require('./views');
 var $ = require('jquery');
+/**
+ * Include views to be called by the router here each should
+ * take a callback that takes in data, html
+ * and returns a promise which is resolved after its loaded.
+ * @module edit_views
+ */
 
-// REWRITE ALL OF THESE TO SHOW THE EDIT TEMPLATES NOT THE NORMAL ONES
+/**
+ * Main view for rendering components and making them editable.
+ * @param {object} match - match object Returned by routes.
+ * @param {function} callback - callback is called with html
+ * @returns {promise} Resolves when complete
+ */
 exports.displayMainContent = function(match, callback) {
   callback = callback ? callback : function() {};
   var component;
@@ -9,20 +20,38 @@ exports.displayMainContent = function(match, callback) {
     component = data;
     html += exports._socialSharingElement(component);
     callback(data, html);
-  };
-  views.displayMainContent(match, cb).then(function() {
     exports._makeEditable(component);
-  });
+  };
+  return views.displayMainContent(match, cb);
 };
 
+/**
+ * View for rendering the homepage and making it editable.
+ * @param {object} match - match object Returned by routes.
+ * @param {function} callback - callback is called with html
+ * @returns {promise} Resolves when complete
+ */
 exports.displayHomepage = function(match, callback) {
-  views.displayHomepage(match, callback).then(function() {
+  callback = callback ? callback : function() {};
+  var homepage;
+  var cb = function(data, html) {
+    homepage = data;
+    html += exports._socialSharingElement(component);
+    callback(data, html);
     //make editable somehow
     // probably calling make editable on it? need to review the
     // splash page json
-  });
+  };
+  return views.displayHomepage(match, cb);
 };
 
+/**
+ * helper function. If a single component, makes it's metadata, attributes
+ * and main data editable. If an array (a list) makes the array sortable
+ * adds a save buttton to the component
+ * @param {component} component - The component we're making editable
+ * @returns {void}
+ */
 exports._makeEditable = function(component) {
   if (Object.prototype.toString.call( component ) === '[object Array]') {
     exports._makeListEditable(component);
@@ -38,15 +67,33 @@ exports._makeEditable = function(component) {
   $('#' + component.slug).append(exports._createSaveButton(component));
 };
 
-exports._makeListEditable = function(array) {
-  console.log(array);
+/**
+ * helper function. Makes an array sortable
+ * @param {array} components - The component array we're making sortable
+ * @returns {void}
+ */
+exports._makeListEditable = function(components) {
+  console.log(components);
   // uhhh. welp
 };
 
+/**
+ * helper function. Makes data editable
+ * @param {component} component - The component who's data we're making editable
+ * @returns {void}
+ */
 exports._editableData = function(component) {
   console.log(component);
   // uhhh. welp
 };
+
+/**
+ * helper function. Makes a metadata html element editable, and makes
+ * changes on the component object when the content changes.
+ * @param {component} component - The component who's metadata we're making editable
+ * @param {string} meta - The name of the metadata we're making editable
+ * @returns {void}
+ */
 exports._editableMetadata = function(component, meta) {
   $('.' + component.slug + '.' + meta)
     .attr('contentEditable', true)
@@ -56,9 +103,22 @@ exports._editableMetadata = function(component, meta) {
     });
 };
 
+/**
+ * helper function. creates a panel from which editors can change component
+ * metadata related to social sharing
+ * @param {component} component - The component who's social data we're editing
+ * @param {string} meta - The name of the metadata we're making editable
+ * @returns {string} html - the raw html of the social sharing element
+ */
 exports._socialSharingElement = function(component) {
   return "<div><h4>this could one day be an edit popup or something" + component.slug + "</h4></div>";
 };
+
+/**
+ * helper function. creates the button that updates a component on click
+ * @param {component} component - The component that will be updated
+ * @returns {jQuery element} button - the jQuery element that will update a component on click
+ */
 exports._createSaveButton = function(component) {
   return $('<button>Save</button>')
     .click(function() {
@@ -70,12 +130,21 @@ exports._createSaveButton = function(component) {
   );
 };
 
-exports._successNotice = function() {
-  // put up a flickering banner saying we saved or something
-  console.log('updated successfully');
+/**
+ * helper function. Runs when a component is successfully updated
+ * eventually we want this to put up a banner saying 'saved' that fades out
+ * @param {string} message - The success message
+ */
+exports._successNotice = function(message) {
+  console.log('updated successfully', message);
 };
 
+/**
+ * helper function. Runs when a component fails to update
+ * eventually we want this to put up a banner saying 'failed' that fades out
+ * we should consider replacing this w/ the EnvConfig error handling
+ * @param {string} message - The success message
+ */
 exports._successNotice = function(error) {
-  // put up a flickering banner saying we failed
   console.log('updating failed!', error);
 };
