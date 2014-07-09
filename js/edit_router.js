fd0efdaca0d1dde56d91b8a4078af7b1bf1a4708
Ben Breedlove <benbreedlove@gmail.com>
2014-07-09 13:53:30 -0700

    add some thoughts on how to proceed on making editable to edit router

--- js/edit_router.js
+++ js/edit_router.js
@@ -7,6 +7,9 @@ router.addRoute("^/$", views.displayHomepage);
 router.addRoute(":schema/:slug", views.displayMainContent);
 
   /* ADD EDIT SPECIFIC POST PAGE LOAD STUFF HERE
+   * if we can make the make editable fucntion smarter, we can ditch
+   * overwriting the normal views w/ edit views, and make things editable
+   * by putting the edit function here in the callback
    *   module.exports.callback = function() {
    *     // (setting up content editable, for instnace)
    *   }
