9b8b9bf6c9f76f2bb3c037f8914a47e0c7ff8274
Ben Breedlove <benbreedlove@gmail.com>
2014-10-28 12:59:20 -0700

    strikethrough can have bold or italic within it

--- js/smoke_tweditor.js
+++ js/smoke_tweditor.js
@@ -63,10 +63,9 @@ exports.headerButton = function(editor) {
 exports.makeStrikethroughButton = function(editor) {
   var strikethroughButton = $('<li class="editButton"><i class="fa fa-strikethrough"></i></li>');
   strikethroughButton.on("click", function() {
-    var newText = editor.getSelection().replace('*', '', 'g');
-    newText = newText.replace(/\s*$/, '');
+    var newText = editor.getSelection().replace(/\s*$/, '');
     newText = newText.replace(/^\s*/, '');
-    editor.replaceSelection(' ~~'+newText+'~~ ', "end");
+    editor.replaceSelection('~~'+newText+'~~', "end");
     editor.focus();
   });
   return strikethroughButton;
